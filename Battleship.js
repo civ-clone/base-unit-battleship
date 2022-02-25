48390b71fa7a199af3daefe57d2f17b8fba09f5b
dom111 <dom111@users.noreply.github.com>
2022-02-25 21:13:56 +0000

    Initial commit.

---
+++ Battleship.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Battleship = void 0;
+const Fortifiable_1 = require("@civ-clone/base-unit-type-fortifiable/Fortifiable");
+class Battleship extends Fortifiable_1.Fortifiable {
+}
+exports.Battleship = Battleship;
+exports.default = Battleship;
+//# sourceMappingURL=Battleship.js.map
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-unit-battleship
+
+This is a template repo.
+
